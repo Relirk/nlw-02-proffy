5904cb76525103874b1415d5fa577e19fa3032b4
Francisco <francisco.olvera@finanzero.com.br>
2020-08-04 00:20:10 -0400

    Primeiro dia - 03/08 Acelerando sua evolução

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+import "./styles.css";
+
+const TeacherItem: React.FC = () => {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars3.githubusercontent.com/u/8285478?s=460&u=081e829c0cb7474759b036de0d0aa63b1aec6ab1&v=4"
+          alt="Francisco Olvera"
+        />
+        <div>
+          <strong>Francisco Olvera</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br />
+        <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço por hora
+          <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default TeacherItem;
